93e37b181dcf312f21918c5d752d0594baceb16f
Ghazanfar <ghazanfar.hussain14@gmail.com>
2024-01-05 00:32:42 +0500

    product preview

--- food_app/lib/product_overview.dart
+++ food_app/lib/product_overview.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ProductReview extends StatefulWidget {
-  const ProductReview({super.key});
+  final String productImage;
+  const ProductReview(this.productImage, {super.key});
 
   @override
   State<ProductReview> createState() => _ProductReviewState();
 }
+
+enum Radiovalues { checked, unchecked }
+
+Radiovalues val = Radiovalues.unchecked;
 
 class _ProductReviewState extends State<ProductReview> {
   @override
@@ -13,6 +18,80 @@ class _ProductReviewState extends State<ProductReview> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Product Review"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text("Fresh Basil")),
+          Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text("50\$")),
+          Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+            child: Image.network(
+              widget.productImage,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text("Available Option"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Radio(
+                    value: val,
+                    groupValue: val,
+                    onChanged: (valu) {
+                      setState(() {
+                        val = valu!;
+                      });
+                    }),
+              ),
+              Text("50\$"),
+              Container(
+                  height: 40,
+                  width: 80,
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.yellow),
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      )),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Icon(Icons.add), Text("Add")],
+                  ))
+            ],
+          )
+        ],
+      ),
+      bottomNavigationBar: Row(
+        children: [
+          Expanded(
+              child: Container(
+            height: 50,
+            color: Colors.grey,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Icon(Icons.favorite), Text("Add to WishList")],
+            ),
+          )),
+          Expanded(
+              child: Container(
+            height: 50,
+            color: Colors.yellow,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Icon(Icons.shopping_cart), Text("Go to Cart")],
+            ),
+          ))
+        ],
       ),
     );
   }
--- food_app/lib/single_product.dart
+++ food_app/lib/single_product.dart
@@ -4,8 +4,8 @@ import 'product_overview.dart';
 class SingleProduct extends StatelessWidget {
   final String productImage;
   final String productName;
-  final Function onTap;
-  const SingleProduct(this.productImage, this.productName, this.onTap,
+  final Function onTapIn;
+  const SingleProduct(this.productImage, this.productName, this.onTapIn,
       {super.key});
 
   @override
@@ -14,77 +14,69 @@ class SingleProduct extends StatelessWidget {
       margin: EdgeInsets.all(10),
       child: Row(
         children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const ProductReview()),
-              );
-            },
-            child: Container(
-              height: 300,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Colors.amber.shade200,
-                  borderRadius: BorderRadius.circular(20)),
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 20),
-                    width: 200,
-                    child: ClipRRect(
-                      child: Image.network(productImage),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+          Container(
+            height: 300,
+            width: 250,
+            decoration: BoxDecoration(
+                color: Colors.amber.shade200,
+                borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  width: 200,
+                  child: ClipRRect(
+                    child: Image.network(productImage),
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  Container(
-                    margin: EdgeInsets.only(left: 25, top: 20, bottom: 10),
-                    child: Row(
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(productName),
-                            Text("50\$/50Gram"),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 25, top: 20, bottom: 10),
+                  child: Row(
                     children: [
-                      Container(
-                          decoration: BoxDecoration(
-                              color: Colors.red,
-                              border: Border.all(color: Colors.black),
-                              borderRadius: BorderRadius.circular(20)),
-                          child: Row(
-                            children: [
-                              Text("50grams"),
-                              Icon(Icons.arrow_drop_down)
-                            ],
-                          )),
-                      Container(
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(productName),
+                          Text("50\$/50Gram"),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
                         decoration: BoxDecoration(
-                          color: Colors.red,
-                          border: Border.all(color: Colors.black),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
+                            color: Colors.red,
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(20)),
                         child: Row(
                           children: [
-                            Icon(Icons.remove),
-                            Text("1"),
-                            Icon(Icons.add)
+                            Text("50grams"),
+                            Icon(Icons.arrow_drop_down)
                           ],
-                        ),
+                        )),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                    ],
-                  )
-                ],
-              ),
+                      child: Row(
+                        children: [
+                          Icon(Icons.remove),
+                          Text("1"),
+                          Icon(Icons.add)
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
             ),
-          )
+          ),
         ],
       ),
     );
